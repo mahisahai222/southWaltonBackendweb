028ab72543cf99cf98554a86abdc486a1f01a3c2
unknown <akashtripathi2002@gmail.com>
2024-10-14 16:40:33 +0530

    agree

--- routes/signRoute.js
+++ routes/signRoute.js
@@ -1,13 +1,24 @@
 
+// const express = require('express');
+// const router = express.Router();
+// const signController = require('../controllers/signController');
+
+// router.post('/save', signController.saveSignature);
+// router.get('/:userId', signController.getSignature);
+// router.get('/', signController.getAllSignatures); //getAll
+// router.delete(':userId', signController.deleteSignature);
+// router.get('/image/:userId', signController.getSignatureImage);
+
+
+// module.exports = router;
 const express = require('express');
+const { saveImageUrl } = require('../controllers/signController');
+const multer = require('multer');
+
 const router = express.Router();
-const signController = require('../controllers/signController');
+const upload = multer({ dest: 'uploads/' }); // Use the same storage configuration as above
 
-router.post('/save', signController.saveSignature);
-router.get('/:userId', signController.getSignature);
-router.get('/', signController.getAllSignatures); //getAll
-router.delete(':userId', signController.deleteSignature);
-router.get('/image/:userId', signController.getSignatureImage);
-
+// Route to save image URL with user ID
+router.post('/save', saveImageUrl); // 'image' is the key for the file input
 
 module.exports = router;
