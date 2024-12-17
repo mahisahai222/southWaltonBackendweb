e049387ced341b746d5577221e3ca7b957384072
UtkarshAayan <utkarsh.gupta@aayaninfotech.com>
2024-12-17 12:16:59 +0530

    hidekeys

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,41 +2,12 @@
 const multer = require('multer');
 const { S3 } = require('@aws-sdk/client-s3');
 const path = require('path');
-require('dotenv').config();
-const AWS = require('aws-sdk');
-
-
-const secretsManager = new AWS.SecretsManager({ region: process.env.AWS_REGION });
-
-async function getAwsCredentials() {
-  try {
-    const data = await secretsManager.getSecretValue({ SecretId: 'AWS-utk' }).promise();
-    if (data.SecretString) {
-      const secrets = JSON.parse(data.SecretString);
-      console.log('AWS Credentials:', secrets);
-      return secrets;
-    }
-  } catch (error) {
-    console.error('Error fetching secrets:', error.message);
-  }
-}
-
-getAwsCredentials().then(credentials => {
-  if (credentials) {
-    AWS.config.update({
-      accessKeyId: credentials.AWS.ACCESS_KEY_ID,
-      secretAccessKey: credentials.AWS.SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION
-    });
-  }
-});
-
 
 // Configure AWS S3
 const s3 = new S3({
   credentials: {
-    accessKeyId: credentials.AWS.ACCESS_KEY_ID,
-    secretAccessKey: credentials.AWS.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
   region: process.env.AWS_REGION,
 });
