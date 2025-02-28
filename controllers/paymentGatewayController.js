2df0aa6b40939ba1ca428c18eb01535bb3ea7a92
UtkarshAayan <utkarsh.gupta@aayaninfotech.com>
2025-02-28 15:39:52 +0530

    reservationpay

--- controllers/paymentGatewayController.js
+++ controllers/paymentGatewayController.js
@@ -11,8 +11,8 @@ const createCheckoutSession = async (req, res) => {
     try {
         const { amountInDollars, userId, bookingId, reservation, fromAdmin, paymentType } = req.body;
 
-        if (!amountInDollars || !userId  || !reservation || !fromAdmin || !paymentType) {
-            return res.status(400).json({ error: "All fields (amount, userId, reservation,fromAdmin,paymentType) are required" });
+        if (!amountInDollars || !reservation || !fromAdmin || !paymentType) {
+            return res.status(400).json({ error: "All fields (amountInDollars, reservation,fromAdmin,paymentType) are required" });
         }
 
         const session = await stripe.checkout.sessions.create({
